de86894aa27b9be4ba92db984154f0c4f2413c80
vitaliyzahrai <vitaliy.zahrai@gmail.com>
2020-12-01 23:25:12 +0200

    add page

---
+++ README.md
@@ -0,0 +1,2 @@
+# snouborde
+snouborde
---
+++ script.js
@@ -0,0 +1,41 @@
+$('.sl_carousel_snow').slick({
+	centerMode: true,
+  centerPadding: '135px',
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  adaptiveHeight: true,
+  // autoplay: true,
+  // autoplaySpeed: 2000,
+  pauseOnHover:true,
+  pauseOnFocus:true,
+  focusOnSelect: true,
+  responsive: [
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 740,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        centerMode: false
+      }
+    },
+    {
+      breakpoint: 560,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: false
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+
+});
